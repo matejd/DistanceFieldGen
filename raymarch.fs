dbe60c97455f9f7b14b616d1e5841d2490f51762
Matej Drame <matej.drame@gmail.com>
2014-11-27 19:12:41 +0100

    Explained a couple of lines in the fragment shader better.

--- raymarch.fs
+++ raymarch.fs
@@ -6,13 +6,15 @@ uniform float time;
 
 float sampleDistanceField(vec3 position)
 {
-   vec3 coords = clamp(position, 0.0, 1.0);
-   vec2 c1 = vec2((floor(coords.z*distFieldSize) + coords.x) / distFieldSize,       coords.y);
-   vec2 c2 = vec2((floor(coords.z*distFieldSize) + 1.0 + coords.x) / distFieldSize, coords.y);
-   float d1 = texture2D(distFieldSam, c1).r - 0.5;
-   float d2 = texture2D(distFieldSam, c2).r - 0.5;
-   float a = fract(coords.z*distFieldSize);
-   return mix(d1, d2, a);
+    // Sample at two points (two y slices) and interpolate. This emulates
+    // bilinear interpolation for 3D texture.
+    vec3 coords = clamp(position, 0.0, 1.0);
+    vec2 c1 = vec2((floor(coords.z*distFieldSize) + coords.x) / distFieldSize,       coords.y);
+    vec2 c2 = vec2((floor(coords.z*distFieldSize) + 1.0 + coords.x) / distFieldSize, coords.y);
+    float d1 = texture2D(distFieldSam, c1).r - 0.5; // Assuming signed distance field (values from -0.5 to 0.5).
+    float d2 = texture2D(distFieldSam, c2).r - 0.5;
+    float a = fract(coords.z*distFieldSize);
+    return mix(d1, d2, a);
 }
 
 float rand(vec3 co)
@@ -33,7 +35,7 @@ float march(vec3 ro, vec3 rd, float randOff)
             all(greaterThan(p, vec3(0.0)))) {
             float dist = sampleDistanceField(p);
             if (dist < 0.0)
-                result += 4.0 / 20.0; // Count samples "inside" the mesh.
+                result += 4.0/20.0; // Count samples "inside" the mesh.
         }
         t += sqrt(2.0)/20.0;
     }
@@ -47,13 +49,17 @@ void main()
     float halfHeight = canvasSize.y / 2.f;
     float x = (gl_FragCoord.x - halfWidth)  / canvasSize.x;
     float y = ratio * (gl_FragCoord.y - halfHeight) / canvasSize.y;
+
+    // Random offset - value between 0 and 0.2.
     float randOff = rand(vec3(gl_FragCoord.xy / canvasSize, time)) * 0.2;
 
+    // Get ray origin and ray direction from camera position (origin) and
+    // target (0.5, 0.5, 0.5).
     vec3 ro = origin.xzy; // Flip y and z.
     vec3 ww = normalize(vec3(0.5)-ro);
     vec3 uu = normalize(cross(ww, vec3(0.0, 1.0, 0.0)));
     vec3 vv = normalize(cross(uu, ww));
-	vec3 rd = normalize(x*uu + y*vv + 2.5*ww);
+    vec3 rd = normalize(x*uu + y*vv + 2.5*ww);
 
     float t = march(ro, rd, randOff);
 
@@ -63,7 +69,7 @@ void main()
     float yn = y/ratio+0.5;
     vec2 dist = vec2(0.5-xn,0.5-yn);
     float strength = 1.0 - dot(dist, dist);
-    color = randOff * pow(strength, 3.0) * vec3(1.0);
+    color = randOff * pow(strength, 3.0) * vec3(1.0); // Noise in the background as well.
     color += vec3(0.0, t, 0.0);
     gl_FragColor = vec4(color, 1.0);
 }
